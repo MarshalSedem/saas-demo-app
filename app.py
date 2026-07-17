251495d0d05791b476ad03a3775bc3a25eb237df
MarshalSedem <161119991+MarshalSedem@users.noreply.github.com>
2026-07-17 14:07:12 +0000

    Update homepage heading to test auto-deploy

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return f"""
-    <h1>Innovartus SaaS Demo</h1>
+    <h1>Innovartus SaaS Demo v2</h1>
     <p>Status: Running</p>
     <p>Server time: {datetime.datetime.now()}</p>
     """
